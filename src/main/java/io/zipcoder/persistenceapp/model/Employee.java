ab222f0b0cf36131fa5aeb1900f05dac98a355c5
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-04-19 23:23:02 -0400

    Employee getters and setters

--- src/main/java/io/zipcoder/persistenceapp/model/Employee.java
+++ src/main/java/io/zipcoder/persistenceapp/model/Employee.java
@@ -31,5 +31,77 @@ public class Employee {
         this.hireDate = hireDate;
         Manger = manger;
     }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getManger() {
+        return Manger;
+    }
+
+    public void setManger(String manger) {
+        Manger = manger;
+    }
 }
 
