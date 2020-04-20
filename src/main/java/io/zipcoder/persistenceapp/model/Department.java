1e57fe4aa224b14cff7762db6227a7b7a89ee992
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-04-19 23:26:09 -0400

    Department Entity, getters and setters

---
+++ src/main/java/io/zipcoder/persistenceapp/model/Department.java
@@ -0,0 +1,45 @@
+package io.zipcoder.persistenceapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+
+    @Id
+    int departmentNumber;
+    String departmentName;
+    String departmentManager;
+
+    public Department(){}
+
+    public Department(int departmentNumber, String departmentName, String departmentManager) {
+        this.departmentNumber = departmentNumber;
+        this.departmentName = departmentName;
+        this.departmentManager = departmentManager;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentManager() {
+        return departmentManager;
+    }
+
+    public void setDepartmentManager(String departmentManager) {
+        this.departmentManager = departmentManager;
+    }
+}
